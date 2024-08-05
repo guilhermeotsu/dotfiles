cd1cd0cc06ebb3f8e95ed0c027232813661d8a78
guilherme otsu <guiotsu@gmail.com>
2024-08-05 15:39:42 -0300

    nvim: move config autocmd from ftplugin to zen-mode.lua

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,1 @@
 vim.opt.numberwidth=4
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { "*.md" },
-  callback = require("zen-mode").open,
-})
--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -2,5 +2,11 @@ return {
   {
     "folke/zen-mode.nvim",
     opts = {},
+    config = function()
+      vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        pattern = { "*.md" },
+        callback = require("zen-mode").open,
+      })
+    end,
   },
 }
