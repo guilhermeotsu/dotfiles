5a43d3bf892379a138d0c74245976df9b64fda9e
guilherme otsu <guiotsu@gmail.com>
2024-08-05 19:10:15 -0300

    nvim: add config for cs.lua

--- nvim/after/ftplugin/cs.lua
+++ nvim/after/ftplugin/cs.lua
@@ -1,1 +1,14 @@
-vim.opt.tabstop              = 4
+vim.opt.shiftwidth = 4
+
+vim.cmd[[
+  function! AutoBracketDrop()
+    if col('.') == col('$') - 1
+      substitute /\s*$//
+      exec "normal! A\<Cr>{\<Cr>X\<Cr>}\<Esc>k$x"
+    else
+      exec "normal! a{\<Cr>\<Esc>"
+    endif
+  endfunction
+]]
+
+vim.keymap.set('i', '{<cr>', '<Esc>:call AutoBracketDrop()<cr>')
