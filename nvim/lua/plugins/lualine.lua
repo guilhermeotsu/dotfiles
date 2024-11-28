c9ed2bbadd30ffa99a0243f1c069540be72d4ec2
Guilherme Otsu <guiotsu@gmail.com>
2024-11-28 14:03:57 -0300

    feat(nvim): add lsp progress on lualine plugin

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,51 @@
+-- https://github.com/linrongbin16/lsp-progress.nvim/pull/133/files#diff-7bb74865bd40076b8da8648f3a65195d7d4728c862077e033d111b4bdf47c790
 return {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  config = function()
-    require('lualine').setup()
+  "nvim-lualine/lualine.nvim",
+  event = "VimEnter",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    {
+      "linrongbin16/lsp-progress.nvim",
+      opts = {},
+    },
+  },
+  config = function(_, opts)
+    require("lualine").setup(opts)
+
+    vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = "lualine_augroup",
+      pattern = "LspProgressStatusUpdated",
+      callback = require("lualine").refresh,
+    })
   end,
+  opts = {
+    options = {
+      component_separators = { left = "|", right = "|" },
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return str:sub(1, 1)
+          end,
+        },
+      },
+      lualine_b = {
+        { "branch", icons_enabled = false },
+      },
+      lualine_c = {
+        function()
+          return require("lsp-progress").progress()
+        end,
+      },
+    },
+    tabline = {
+      lualine_a = {
+        "buffers",
+      },
+    },
+  },
 }
